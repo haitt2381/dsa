4dab4e3377910fa8b3427caa8a85f4d59457eb57
HaiTT23 <haitt2381@gmail.com>
2025-05-07 15:36:52 +0700

    Implement print List method

---
+++ src/main/java/com/haitt/linkedlist/singly/SinglyLinkedList.java
@@ -0,0 +1,37 @@
+package com.haitt.linkedlist.singly;
+
+import com.haitt.linkedlist.Node;
+
+import java.util.Objects;
+
+public class SinglyLinkedList {
+    Node head;
+
+
+    /**
+     * Returns a string representation of the linked list. If the list is empty,
+     * returns a message indicating the list is empty
+     *
+     * @return A string representing the list, or <code>The list is empty</code> if the list is empty.
+     */
+    public String print() {
+        if (Objects.isNull(head)) {
+            return "The list is empty";
+        }
+        
+        Node curr = head;
+        StringBuilder result = new StringBuilder();
+        
+        while (Objects.nonNull(curr)) {
+            result.append(curr.data);
+            result.append(" -> ");
+            curr = curr.next;
+        }
+        
+        result.append("null");
+        
+        return result.toString();
+    }
+    
+    
+}
---
+++ src/main/java/com/haitt/linkedlist/singly/main.java
@@ -0,0 +1,20 @@
+package com.haitt.linkedlist.singly;
+
+import com.haitt.linkedlist.Node;
+
+public class main {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        
+        list.head = first;
+        first.next = second;
+        second.next = third;
+        
+        //PrintList exercise
+        System.out.println(list.print());
+    }
+}
